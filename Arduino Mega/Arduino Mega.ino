42be489d27da60c08338d481b51bd97cbb236e88
kanavdawra <kanavdawra97@gmail.com>
2017-12-08 13:02:58 +0530

    Add files via upload

---
+++ Arduino Mega/Arduino Mega.ino
@@ -0,0 +1,64 @@
+
+char str[16]="11111111111";
+int a0=23,a1=25,a2=27,a3=29,a4=31,a5=33,a6=35,a7=37,d0=39,d1=41,d2=43,d3=45;
+void setup() {
+Serial.begin(115200);
+
+}
+
+void loop() {
+int i=0;
+
+  if (Serial.available()) {
+    delay(100); //allows all serial sent to be received together
+    while(Serial.available() && i<16) {
+      str[i++] = Serial.read();
+    }
+    str[i++]='\0';
+    if(i>0) {
+    if(str[0]=='p' && str[1]=='u' && str[2]=='b'){
+    Serial.print("Massage");Serial.println(str);
+    if(str[3]=='0'){pinMode(a0,OUTPUT);digitalWrite(a0, 0);Serial.print("a0:");Serial.println(str[3]);}
+    else if(str[3]=='1'){pinMode(a0,OUTPUT);digitalWrite(a0, 1);Serial.print("a0:");Serial.println(str[3]);}
+    if(str[4]=='0'){pinMode(a1,OUTPUT);digitalWrite(a1, 0);Serial.print("a1:");Serial.println(str[4]);}
+    else if(str[4]=='1'){pinMode(a1,OUTPUT);digitalWrite(a1, 1);Serial.print("a1:");Serial.println(str[4]);}
+    if(str[5]=='0'){pinMode(a2,OUTPUT);digitalWrite(a2, 0);Serial.print("a2:");Serial.println(str[5]);}
+    else if(str[5]=='1'){pinMode(a2,OUTPUT);digitalWrite(a2, 1);Serial.print("a2:");Serial.println(str[5]);}
+    if(str[6]=='0'){pinMode(a3,OUTPUT);digitalWrite(a3, 0);Serial.print("a3:");Serial.println(str[6]);}
+    else if(str[6]=='1'){pinMode(a3,OUTPUT);digitalWrite(a3, 1);Serial.print("a3:");Serial.println(str[6]);}
+    if(str[7]=='0'){pinMode(a4,OUTPUT);digitalWrite(a4, 0);Serial.print("a4:");Serial.println(str[7]);}
+    else if(str[7]=='1'){pinMode(a4,OUTPUT);digitalWrite(a4, 1);Serial.print("a4:");Serial.println(str[7]);}
+    if(str[8]=='0'){pinMode(a5,OUTPUT);digitalWrite(a5, 0);Serial.print("a5:");Serial.println(str[8]);}
+    else if(str[8]=='1'){pinMode(a5,OUTPUT);digitalWrite(a5, 1);Serial.print("a5:");Serial.println(str[8]);}
+    if(str[9]=='0'){pinMode(a6,OUTPUT);digitalWrite(a6, 0);Serial.print("a6:");Serial.println(str[9]);}
+    else if(str[9]=='1'){pinMode(a6,OUTPUT);digitalWrite(a6, 1);Serial.print("a6:");Serial.println(str[9]);}
+    if(str[10]=='0'){pinMode(a7,OUTPUT);digitalWrite(a7, 0);Serial.print("a7:");Serial.println(str[10]);}
+    else if(str[10]=='1'){pinMode(a7,OUTPUT);digitalWrite(a7, 1);Serial.print("a7:");Serial.println(str[10]);}
+    if(str[11]=='0'){pinMode(d0,OUTPUT);digitalWrite(d0, 0);Serial.print("d0:");Serial.println(str[11]);}
+    else if(str[11]=='1'){pinMode(d0,OUTPUT);digitalWrite(d0, 1);Serial.print("d0:");Serial.println(str[11]);}
+    if(str[12]=='0'){pinMode(d1,OUTPUT);digitalWrite(d1, 0);Serial.print("d1:");Serial.println(str[12]);}
+    else if(str[12]=='1'){pinMode(d1,OUTPUT);digitalWrite(d1, 1);Serial.print("d1:");Serial.println(str[12]);}
+    if(str[13]=='0'){pinMode(d2,OUTPUT);digitalWrite(d2, 0);Serial.print("d2:");Serial.println(str[13]);}
+    else if(str[13]=='1'){pinMode(d2,OUTPUT);digitalWrite(d2, 1);Serial.print("d2:");Serial.println(str[13]);}
+    if(str[14]=='0'){pinMode(d3,OUTPUT);digitalWrite(d3, 0);Serial.print("d3:");Serial.println(str[14]);}
+    else if(str[14]=='1'){pinMode(d3,OUTPUT);digitalWrite(d3, 1);Serial.print("d3:");Serial.println(str[14]);}
+    pinMode(48,OUTPUT);digitalWrite(48, 1);
+    delay(500);
+    digitalWrite(48, 0);
+    Serial.println(str);
+   i=-1;
+    
+  }
+  if(str[0]=='c' && str[1]=='o' && str[2]=='n' && str[3]=='f'){pinMode(40,OUTPUT);digitalWrite(40, 1);Serial.println(str);}
+  if(str[0]=='r' && str[1]=='e' && str[2]=='s' && str[3]=='t'){pinMode(44,OUTPUT);
+  
+  digitalWrite(40, 0);
+  delay(300);
+  digitalWrite(44, 1);
+  delay(500);
+  digitalWrite(44, 0);
+  Serial.println(str);}
+    }
+  }
+  
+}
---
+++ Esp8266/Esp8266.ino
@@ -0,0 +1,139 @@
+
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <PubSubClient.h>
+//needed for library
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>    
+ #include<SoftwareSerial.h> 
+SoftwareSerial s(3,1); 
+const int PIN_LED = 0;
+const int TRIGGER_PIN = 2;
+const int TRIGGER_PIN2 = 13; 
+bool initialConfig = false;
+const char* mqttServer = "####";
+const int mqttPort = ####;
+const char* mqttUser = "####";
+const char* mqttPassword = "####";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.print("rest");
+  Serial.print("rest+");
+  Serial.print("pub");
+  Serial.print("rest");
+  Serial.print("rest");
+  //Serial.println("\n Starting");
+//  WiFi.printDiag(Serial); //Remove this line if you do not want to see WiFi password printed
+  if (WiFi.SSID()==""){
+   // Serial.println("We haven't got any access point credentials, so get them now");   
+    initialConfig = true;
+  }
+  
+  else{
+    digitalWrite(PIN_LED, LOW); // Turn led off as we are not in configuration mode.
+    WiFi.mode(WIFI_STA); // Force to station mode because if device was switched off while in access point mode it will start up next time in access point mode.
+    unsigned long startedAt = millis();
+   // Serial.print("After waiting ");
+    int connRes = WiFi.waitForConnectResult();
+    float waited = (millis()- startedAt);
+   /* Serial.print(waited/1000);
+    Serial.print(" secs in setup() connection result is ");
+    Serial.println(connRes);*/
+  }
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+  pinMode(TRIGGER_PIN2, INPUT_PULLUP);
+  if (WiFi.status()!=WL_CONNECTED){
+   // Serial.println("failed to connect, finishing setup anyway");
+  } else{
+   // Serial.print("local ip: ");
+   // Serial.println(WiFi.localIP());
+
+//mqtt//
+
+    client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    //Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("esp8266", mqttUser, mqttPassword )) {
+ 
+      //Serial.println("connected");  
+ digitalWrite(PIN_LED, HIGH);
+    } else {
+ 
+     // Serial.print("failed with state ");
+      //Serial.print(client.state());
+      delay(1000);
+      Serial.print("restmqtt");
+      ESP.restart();
+ 
+    }
+  }
+ 
+  client.publish("data", "Hello from ESP8266");
+  client.subscribe("data");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ char abc[16];
+  //Serial.print("Message arrived in topic: ");
+  //Serial.println(topic);
+ //Serial.write(payload,length);
+  //Serial.print("Message:");
+  abc[0]='p';abc[1]='u';abc[2]='b';
+  for (int i = 0; i < length; i++) {
+    //Serial.print((char)payload[i]);
+    abc[i+3]=(char)payload[i];
+    
+  }
+ Serial.println(abc);
+ s.write(abc,16);
+  //Serial.println();
+  //Serial.println("-----------------------");
+ 
+}
+
+
+void loop() {char str[16];
+  // is configuration portal requested?
+ 
+  if ((digitalRead(TRIGGER_PIN) == LOW) || (digitalRead(TRIGGER_PIN2) == LOW) || (initialConfig)) {
+     //Serial.println("Configuration portal requested.");
+   /* for(int x=0;x<10;x++)
+    {digitalWrite(PIN_LED, LOW);
+    delay(500);
+    digitalWrite(PIN_LED, HIGH);
+    delay(100);}*/
+     digitalWrite(PIN_LED, LOW);
+     //#############configration bortal led serial print#################
+     str[0]='ç';str[1]='o';str[2]='n';str[3]='f';
+     Serial.println("conf");
+     
+     
+    WiFiManager wifiManager;
+
+    if (!wifiManager.startConfigPortal("Matrix HomeAuto","password")) {
+      //Serial.println("Not connected to WiFi but continuing anyway.");
+    } else {
+      //Serial.println("connected...yeey :)");
+    }
+    digitalWrite(PIN_LED, HIGH); // Turn led off as we are not in configuration mode.
+    ESP.restart(); 
+    delay(5000);
+  }
+  if(WiFi.status() != WL_CONNECTED || client.connected()!=1){Serial.print("restwifi/mqtt");delay(1000);ESP.restart();}
+if(WiFi.status() == WL_CONNECTED){client.loop();}
+
+
+  // put your main code here, to run repeatedly:
+
+}
